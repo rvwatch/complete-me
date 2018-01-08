3b739e5c2a867f283214024c1b97b5f175e3a312
Ricardo Viera <ricardoclean@gmail.com>
2018-01-08 11:17:52 -0700

    Updating more naming issues

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const trie = require('./lib/CompleteMe.js');
-const node = require('./lib/node.js');
+const Trie = require('./lib/CompleteMe.js');
+const Node = require('./lib/node.js');
 
-module.exports = { trie, node };
+module.exports = { Trie, Node };
